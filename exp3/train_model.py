cf5c8eb210fa4e82e56de5b60aaa4311154f3836
draperrr <yugonxanax@gmail.com>
2026-03-10 10:26:01 +0530

    .

--- exp3/train_model.py
+++ exp3/train_model.py
@@ -1,22 +1,26 @@
 import pandas as pd
+import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-import pickle
 
+# Load dataset
 data = pd.read_csv("HeartDis.csv")
 
+# Features and target
 X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
 
+# Split dataset
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=14
 )
 
-model = LogisticRegression(max_iter=200)
+# Train model
+model = LogisticRegression(max_iter=1000)
 model.fit(X_train, y_train)
 
 # Save model
 with open("model.pkl", "wb") as f:
     pickle.dump(model, f)
 
-print("Model saved as model.pkl")
+print("Model trained and saved as model.pkl")
